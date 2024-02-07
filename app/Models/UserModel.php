fd14cf36995e8388eac1a2f84f0fceba73f76541
fas-fernando <benesinho14@gmail.com>
2024-02-07 09:45:05 -0300

    Aplicado validações nos campos na edição do usuário

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -25,8 +25,34 @@ class UserModel extends Model
     protected $deletedField  = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
+    protected $validationRules      = [
+        'id'                    => 'permit_empty|is_natural_no_zero',
+        'username'              => 'required|min_length[3]|max_length[100]',
+        'email'                 => 'required|valid_email|is_unique[users.email,id,{id}]|max_length[240]',
+        'password'              => 'required|min_length[6]',
+        'password_confirmation' => 'required_with[password]|matches[password]',
+    ];
+    protected $validationMessages   = [
+        'username'              => [
+            'required'   => 'O campo nome é obrigatório',
+            'min_length' => 'O campo nome deve conter no minimo 3 caracteres',
+            'max_length' => 'O campo nome deve conter no máximo 100 caracteres',
+        ],
+        'email'                 => [
+            'required'    => 'O campo email é obrigatório',
+            'valid_email' => 'Informe um email válido',
+            'is_unique'   => 'Esse email já está cadastrado',
+            'max_length'  => 'O campo email deve conter no máximo 240 caracteres',
+        ],
+        'password'              => [
+            'required'    => 'O campo senha é obrigatório',
+            'min_length'  => 'O campo senha deve conter no minimo 6 caracteres',
+        ],
+        'password_confirmation' => [
+            'required_with' => 'Por favor, confirme sua senha',
+            'matches'       => 'As senhas precisam combinar',
+        ],
+    ];
 
     // Callbacks
     protected $beforeInsert   = ['hashPassword'];
