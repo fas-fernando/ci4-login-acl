f6a7f7311dcafd33acae98c7d53a016a5fafd16a
fas-fernando <benesinho14@gmail.com>
2024-02-10 19:02:23 -0300

    Parametrizado alguns métodos na biblioteca de authentication para realizarmos os testes

--- app/Libraries/Authentication.php
+++ app/Libraries/Authentication.php
@@ -27,10 +27,38 @@ class Authentication
         return true;
     }
 
+    public function logout(): void
+    {
+        session()->destroy();
+    }
+
+    public function getUserLogged()
+    {
+        if($this->user === null) $this->user = $this->getUserforSession();
+
+        return $this->user;
+    }
+
+    public function isLogged(): bool
+    {
+        return $this->getUserLogged() !== null;
+    }
+
     private function userLogin(object $user): void
     {
         $session = session();
-        $session->regenerate();
+        $_SESSION['__ci_last_regenerate'] = time();
         $session->set('user_id', $user->id);
+    }
+
+    private function getUserforSession()
+    {
+        if(session()->has('user_id') == false) return null;
+
+        $user = $this->userModel->find(session()->get('user_id'));
+
+        if($user == null || $user->status == null) return null;
+
+        return $user;
     }
 }
